2242e2a5223724bfd49010fba0f9d50504ad6223
Maulidis <82792003+Maulidis@users.noreply.github.com>
2022-08-22 10:53:21 +0700

    lib

---
+++ constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Colors.cyan;
+const kTextColor = Color(0xFF3C4046);
+const kBackgroundColor = Color(0xFFF9F8FD);
+
+
+const  double kDefautPading =20.0;
+
